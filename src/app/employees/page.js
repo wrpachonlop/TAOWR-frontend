206ffb74b9acba14def9c5024534d5aacb437890
Carolina0727 <nunezflorezcarolina@gmail.com>
2025-05-21 11:32:41 -0700

    Le coloqué estilos a la tabla de empleados, agregué el botón de nuevo empleado

--- src/app/employees/page.js
+++ src/app/employees/page.js
@@ -1,7 +1,7 @@
 'use client';
 import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisVertical, faPlus } from '@fortawesome/free-solid-svg-icons'
 export default function EmployeesPage() {  
     const optionsForMenu = [
       {value:1, name: 'View Profile'},
@@ -29,31 +29,41 @@ export default function EmployeesPage() {
   
   const employees=[ //Estoy creando las tablas con la información de los empleados
     { id: 1, CreatedAt : '2025-05-03', UpdatedAt: '2025-05-03', DeletedAt: null, FullName:'Niko Montemayor', Email:'niko@theartofwildroots.com', Phone:'123-456-7890', Address: '123 Main St' , PostalCode: 'V5K0A1' , DriversLicense: 'D1234567' , SIN: '123456789' , BirthDate: '1990-01-01' , IsAdmin: false , IsActive: true , StartDate: "2024-05-01" , JobTitle: 'Owner', TypeContract: 'Full-time', Salary: 50000, InstitutionNo: "001", AccountNo: "123456789",TransitNo: "00011",BankAccountName: "John Doe",Tools: [], Trucks: [],EmergencyContacts: [{ID: 1,CreatedAt: "2025-05-03T17:12:18.716121Z",UpdatedAt: "2025-05-03T17:12:18.716121Z",DeletedAt: null,EmployeeID: 1,Name: "Jane Doe", Phone: "987-654-3210",Address: "456 Another Street",Relationship: "Spouse"}]},
-    { id: 2, CreatedAt : '2025-05-03', UpdatedAt: '2025-05-03',DeletedAt: null, FullName:'Lourdes Hugo', Email:'Lou@theartofwildroots.com', Phone:'123-456-7890', Address: '123 Main St' , PostalCode: 'V5K0A1' , DriversLicense: 'D1234567' , SIN: '123456789' , BirthDate: '1990-01-01' , IsAdmin: false , IsActive: false ,StartDate: "2024-05-01" , JobTitle: 'CEO', TypeContract: 'Full-time', Salary: 50000, InstitutionNo: "001",AccountNo: "123456789",TransitNo: "00011",BankAccountName: "John Doe",Tools: [],Trucks: [],EmergencyContacts: [{ID: 1,CreatedAt: "2025-05-03T17:12:18.716121Z",UpdatedAt: "2025-05-03T17:12:18.716121Z",DeletedAt: null,EmployeeID: 1,Name: "Jane Doe", Phone: "987-654-3210",Address: "456 Another Street",Relationship: "Spouse"}]},
+    { id: 2, CreatedAt : '2025-05-03', UpdatedAt: '2025-05-03',DeletedAt: null, FullName:'Lourdes Hugo', Email:'lou@theartofwildroots.com', Phone:'123-456-7890', Address: '123 Main St' , PostalCode: 'V5K0A1' , DriversLicense: 'D1234567' , SIN: '123456789' , BirthDate: '1990-01-01' , IsAdmin: false , IsActive: false ,StartDate: "2024-05-01" , JobTitle: 'CEO', TypeContract: 'Full-time', Salary: 50000, InstitutionNo: "001",AccountNo: "123456789",TransitNo: "00011",BankAccountName: "John Doe",Tools: [],Trucks: [],EmergencyContacts: [{ID: 1,CreatedAt: "2025-05-03T17:12:18.716121Z",UpdatedAt: "2025-05-03T17:12:18.716121Z",DeletedAt: null,EmployeeID: 1,Name: "Jane Doe", Phone: "987-654-3210",Address: "456 Another Street",Relationship: "Spouse"}]},
   ];      
     return (
       <div className="p-8 m-8">
         <h1 className="text-2xl font-bold mb-4 mt-6">Employees</h1>
-          <div className="overflow-x-auto p-4">  {/* permite hacer scroll si la tabla es muy ancha */}
-          <table className="min-w-full bg-white border border-gray-200">{/*  crea la tabla con todo el ancho disponible con fondo blanco y borde gris*/}
+          <div className='flex justify-end p-4'>
+              <button onClick={()=>alert('Hiciste Click')} className=" block text-left bg-primary font-thin text-white px-3 py-1.5 rounded">
+                <span className='pr-2'><FontAwesomeIcon icon={faPlus} /></span> New Employee
+              </button>
+          </div>
+          <div className=" rounded-lg overflow-x-auto p-4">  {/* permite hacer scroll si la tabla es muy ancha */}
+          <table className="min-w-full bg-white border border-gray-200 mt-8">{/*  crea la tabla con todo el ancho disponible con fondo blanco y borde gris*/}
             <thead className="bg-foreground h-1/2 text-left">{/*  encabezado de la tabla */}
               <tr className='text-background'>
-                <th className="py-2 px-6 border-b">Full Name</th>{/*  py - padding vertical y px - padding horizontal y border-b hace una línea inferior */}
-                <th className="py-2 px-6 border-b">Email</th>
-                <th className="py-2 px-6 border-b">Job Title</th>
-                <th className="py-2 px-6 border-b">Status</th>
-                <th className="border-b"></th>
+                <th className="py-2 px-6 border-b rounded-tl-lg w-1/3">Full Name</th>{/*  py - padding vertical y px - padding horizontal y border-b hace una línea inferior */}
+                <th className="py-2 px-6 border-b w-1/6">Phone Number</th>
+                <th className="py-2 px-4 border-b w-1/8">Job Title</th>
+                <th className="py-2 border-b w-1/8">Status</th>
+                <th className="border-b rounded-tr-lg w-1/20"></th>
               </tr>
             </thead>
             <tbody>
               {employees.map((user) => (
-                <tr key={user.id} className="text-center">{/* En React, cuando haces listas (.map() para crear elementos repetidos como filas de tabla), necesitas darle una key única a cada elemento. Key ayuda a identificar cada fila de manera única (cómo el id del usuario)*/}
-                  <td className="py-2 px-4 border-b">{user.FullName}</td>
-                  <td className="py-2 px-4 border-b">{user.Email}</td>
-                  <td className="py-2 px-4 border-b">{user.JobTitle}</td>
-                  <td className="py-2 px-4 border-b">
-                    {user.IsActive  ? "Active":"Inactive"}</td>
-                  <td className="py-2 px-4 border-b"> 
+                <tr key={user.id} className="">{/* En React, cuando haces listas (.map() para crear elementos repetidos como filas de tabla), necesitas darle una key única a cada elemento. Key ayuda a identificar cada fila de manera única (cómo el id del usuario)*/}
+                  <td className="py-2 pl-8 pr-2 border-b last:rounded-bl-lg text-left border-gray-300">{user.FullName}<br/>
+                    <span className='text-gray-500'>{user.Email}</span></td>
+                  <td className="py-2 px-8 border-b border-gray-300">
+                    <span className="inline-block bg-gray-200 text-sm px-1 py-0.5 rounded-lg">{user.Phone}</span>
+                    </td>
+                  <td className="py-2 px-4 border-b border-gray-300">{user.JobTitle}<br/>
+                    <span className='text-gray-500'>{user.TypeContract}</span>
+                  </td>
+                  <td className="py-2 px-6 border-b border-gray-300">
+                    {user.IsActive  ? <span className="inline-block bg-green-100 text-sm px-1 py-0.5 rounded-lg text-green-800">• Active</span>:<span className="inline-block bg-red-100 text-sm px-1 py-0.5 rounded-lg text-red-800">• Inactive</span>}</td>
+                  <td className="py-2 px-4 border-b last:rounded-br-lg border-gray-300"> 
                     <button onClick={()=>handleClick(user.id)} className="white text-black px-4 py-2 rounded">
                       <FontAwesomeIcon icon={faEllipsisVertical} />
                     </button>
